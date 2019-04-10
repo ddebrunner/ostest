cec11aecff6b93fe94e21962da2ab4d3a07bba41
Dan Debrunner <ddebrunner@users.noreply.github.com>
2019-04-10 15:37:33 -0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
 import os
+import requests
+
+
+
 
 for k in os.environ:
     print(k, os.environ[k])
@@ -9,6 +13,11 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     return "Hello Dan!"
+
+@application.roubt("/pme")
+def pme():
+    x = requests.get('https://postman-echo.com/get?foo1=bar1&foo2=bar2')
+    return x.json()
 
 if __name__ == "__main__":
     application.run()
