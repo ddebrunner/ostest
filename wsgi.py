0abcad505c8ad115b88b608813a8974522f5011f
Dan Debrunner <ddebrunner@users.noreply.github.com>
2019-04-10 15:41:59 -0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,7 @@ import requests
 for k in os.environ:
     print(k, os.environ[k])
     
-from flask import Flask
+from flask import Flask, jsonify
 application = Flask(__name__)
 
 @application.route("/")
@@ -17,7 +17,7 @@ def hello():
 @application.route("/pme")
 def pme():
     x = requests.get('https://postman-echo.com/get?foo1=bar1&foo2=bar2')
-    return x.json()
+    return jsonify(x.json())
 
 if __name__ == "__main__":
     application.run()
