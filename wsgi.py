3481d0919cc7c216738b3f546c53ca8d81676c46
Dan Debrunner <ddebrunner@users.noreply.github.com>
2019-04-10 15:28:20 -0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,14 @@ import os
 
 for k in os.environ:
     print(k, os.environ[k])
+    
+from flask import Flask
+application = Flask(__name__)
+
+@application.route("/")
+def hello():
+    return "Hello Dan!"
+
+if __name__ == "__main__":
+    application.run()
 
